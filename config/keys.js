b4e3677f391d39ce9e83066b75b148a34bff0ce3
Pawel Barul <pawel.barul@programisci.eu>
2018-06-27 22:42:34 +0200

    Express server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://urqel93:lolek123@ds129780.mlab.com:29780/billennium'
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+//@route  GET api/profile/test
+//@desc   Tests profile route
+//@access Public
+router.get('/test', (req, res) => {
+  res.json({
+    message: "PROFILE WORKS"
+  });
+});
+
+module.exports = router;
