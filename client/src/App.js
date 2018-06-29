b14fde8abe00b4318636656f5142da1f0a7b4bed
Pawel Barul <pawel.barul@programisci.eu>
2018-06-29 23:14:48 +0200

    redux login, register

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Provider} from "react-redux";
+import setAuthToken from "./utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import {setCurrentUser} from "./actions/authAction";
+
+import store from "./store"
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
@@ -7,20 +14,32 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import './App.css';
 
+if (localStorage.jwtToken) {
+  //Set auth token header
+  setAuthToken(localStorage.jwtToken);
+  //Decode token and get user info
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+}
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar/>
-          <Route exact path="/" component={Landing}/>
-          <div className="container">
-            <Route exact path="/register" component={Register}/>
-            <Route exact path="/login" component={Login}/>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar/>
+            <Route exact path="/" component={Landing}/>
+            <div className="container">
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+            </div>
+            <Footer/>
           </div>
-          <Footer/>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -24,21 +24,22 @@ router.get('/test', (req, res) => res.json({message: 'WTF MATE?'}));
 router.post('/register', (req, res) => {
   const {errors, isValid} = validateRegisterInput(req.body);
 
-  //Check validation
+  // Check Validation
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  User.findOne({email: req.body.email}).then((user) => {
+  User.findOne({email: req.body.email}).then(user => {
     if (user) {
-      errors.email = "Email already exists"
+      errors.email = 'Email already exists';
       return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
-        s: '200', //size
-        r: 'pg', //rating
-        d: 'mm' //default
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
       });
+
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
@@ -46,7 +47,7 @@ router.post('/register', (req, res) => {
         password: req.body.password
       });
 
-      bcrypt.genSalt(10, (error, salt) => {
+      bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
           if (err) throw err;
           newUser.password = hash;
@@ -54,10 +55,10 @@ router.post('/register', (req, res) => {
             .save()
             .then(user => res.json(user))
             .catch(err => console.log(err));
-        })
-      })
+        });
+      });
     }
-  })
+  });
 });
 
 //@route  GET api/users/login
